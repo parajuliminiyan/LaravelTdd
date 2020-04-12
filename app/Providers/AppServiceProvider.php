8abaf9e2e976da2f5ea76c5c88bec5f350aa5146
Miniyan Parajuli <parajuliminiyan@gmail.com>
2020-04-12 20:37:29 +0545

    [13] View Composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Channel;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
     }
 
     /**
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //       ------------ Sharing Variables toCertain View
+//        \View::composer('threads.create',function($view) {
+//           $view->with('channels',\App\Channel::all());
+//        });
+        //--------------- To all Views-------
+        View::share('channels', Channel::all());
     }
 }
