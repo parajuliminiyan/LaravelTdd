91b8a4175af8992da26a09d7732f9ce7fb8381a6
Miniyan Parajuli <parajuliminiyan@gmail.com>
2020-05-03 13:16:37 +0545

    Last Edited not completed anything

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -7,8 +7,8 @@ use App\Reply;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class FavoritesController extends Controller
-{
+class FavoritesController extends Controller {
+
     /**
      * FavoritesController constructor.
      */
@@ -40,19 +40,21 @@ class FavoritesController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Reply $reply)
     {
-        return $reply->favorite(); 
+        dd($reply);
+        $reply->favorite();
+        return redirect()->back();
 
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -63,7 +65,7 @@ class FavoritesController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -74,8 +76,8 @@ class FavoritesController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -86,7 +88,7 @@ class FavoritesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,20 @@
 <div class="card">
     <div class="card-header">
-        <a href="">{{$reply->owner->name}}</a> said
-        {{$reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h5  class="flex">
+                <a href="#">{{$reply->owner->name}}</a> said
+                {{$reply->created_at->diffForHumans() }}...
+            </h5>
+            <div>
+                <form action="replies/{{$reply->id}}/favorites" method="POST">
+                    @csrf
+                    <button class="btn" type="submit">
+                        {{$reply->favorites()->count()}}
+                        <i class="fas fa-heart"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         {{$reply->body}}
